c071ba5976a7fabd9dff33be5f53dd229238939d
jatinhemnani01 <jatinhemnani01@gmail.com>
2023-12-08 00:57:48 +0530

    google oauth working

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,15 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import Navigation from "@/src/components/Navigation";
+import { SessionProvider } from "next-auth/react";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
+    <SessionProvider>
       <Navigation />
       <Component {...pageProps} />
+      </SessionProvider>
     </>
   );
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,21 @@
 import Navigation from "@/src/components/Navigation";
 import SearchInput from "@/src/components/SearchInput";
 import React from "react";
+import { signIn, signOut, useSession } from "next-auth/react";
 
 export default function Home() {
+  const session = useSession();
+  console.log(session);
+
   return (
     <div>
       <div className="flex justify-center mt-5">
-      <SearchInput />
+        <SearchInput />
+        {session.status === "authenticated" ? (
+          <button onClick={() => signOut()}>Log Out</button>
+        ) : (
+          <button onClick={() => signIn("google")}>Log In</button>
+        )}
       </div>
     </div>
   );
